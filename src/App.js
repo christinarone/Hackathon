aae1c9e29ca6bd0f718ddd4e302f3822851fa2cf
BradLFowler <BradLF00@gmail.com>
2022-07-30 17:56:27 -0500

    got filtered data displayed just needs search input added to api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
 import './App.css';
+import News from './components/News';
 
-function App() {
+const App = (props) => {
+
+  const [filteredNews, setNews] = useState([])
+
+  useEffect(() => {
+    // add searchParam to query=
+    axios.get("http://hn.algolia.com/api/v1/search?query=")
+    .then((res) => {
+      setNews(res.data.hits)
+    })
+  }, [])
+
   return (
     <div className="App">
-<span>:)</span>
+      <div>
+        {filteredNews.map((news, index) => (
+          <News key={index} title={news.title} created_at={news.created_at} author={news.author} url={news.url} points={news.points} 
+          comments={news.num_comments}/>
+        ))}
+      </div>
     </div>
   );
 }
